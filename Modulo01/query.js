d072c74515145b766b2599cf94c75792e323a093
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-17 20:24:14 -0300

    modulo02 README
    Modulo02

---
+++ Modulo01/query.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){    
+    
+
+    $('.gray').hover(function(){
+    $(this).css("background","yellow")
+    })
+
+    $('.green').click(function(){
+
+        $(this).css("background","#ccc")
+    })
+     
+});
+
